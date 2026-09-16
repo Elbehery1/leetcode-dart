557492d603c0d665a3ecd81a5cbab9ade54049ca
Elbehery1 <andrewelbehery153@gmail.com>
2026-09-16 18:26:44 +0300

    Solve Container With Most Water

---
+++ container_with_most_water.dart
@@ -0,0 +1,23 @@
+class Solution {
+  int maxArea(List<int> height) {
+    int left = 0;
+    int right = height.length - 1;
+    int maxWater = 0;
+
+    while (left < right) {
+      int width = right - left;
+      int currentHeight =
+          height[left] < height[right] ? height[left] : height[right];
+      int currentWater = width * currentHeight;
+      if (currentWater > maxWater) {
+        maxWater = currentWater;
+      }
+      if (height[left] < height[right]) {
+        left++;
+      } else {
+        right--;
+      }
+    }
+    return maxWater;
+  }
+}
